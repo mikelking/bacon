8a291c823b4119aa03b34427b589de5cf0ce1792
Mikel King <mikel.king@olivent.com>
2017-04-14 08:04:30 -0400

    Updated the cpt controller to contain the rest of the rest api params and refarctored the debugging.

--- 020-cpt-controller.php
+++ 020-cpt-controller.php
@@ -49,25 +49,28 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * @link https://developer.wordpress.org/resource/dashicons/
  */
 class Custom_Post_Type_Controller {
-	const NAME = '';
-	const SINGULAR_NAME = '';
-	const DESCRIPTION = '';
-	const CAPABILITY_TYPE = 'post';
-	const PUBLICLY_ACCESSIBLE = true;	// label => public
-	const PUBLICLY_QUERYABLE = true;
-	const CUSTOM_ADMIN_UI = true;	// label => show_ui
-	const EXCLUDE_SEARCH = false;	// label => exclude_from_search
-	const ADD_TO_MENUS   = true;	// label => show_in_nav_menus
-	const ADD_TO_ADMIN_MENU = true;	// label => show_in_menu (bool or string)
-	const ADD_TO_ADMIN_BAR = false; // label => show_in_admin_bar
-	const ADD_TO_REST_API = true; // label => show_in_rest
-	const MENU_POSITION = 20;	// label => menu_position (default below pages)
-	const MENU_ICON = null;	// defaults to post icon
-	const META_CAP = true; // map_meta_cap
-	const ARCHIVE_PAGE = true;
-	const EXPORTABLE = true;
-	const TEXT_DOMAIN = '';
-	const DEBUG = false;
+	const NAME                = '';
+	const SINGULAR_NAME       = '';
+	const DESCRIPTION         = '';
+	const CAPABILITY_TYPE     = 'post';
+	const PUBLICLY_ACCESSIBLE = true;  // label => public
+	const PUBLICLY_QUERYABLE  = true;
+	const CUSTOM_ADMIN_UI     = true;  // label => show_ui
+	const EXCLUDE_SEARCH      = false; // label => exclude_from_search
+	const ADD_TO_MENUS        = true;  // label => show_in_nav_menus
+	const ADD_TO_ADMIN_MENU   = true;  // label => show_in_menu (bool or string)
+	const ADD_TO_ADMIN_BAR    = false; // label => show_in_admin_bar
+	const ADD_TO_REST_API     = true;  // label => show_in_rest
+	const MENU_POSITION       = 20;    // label => menu_position (default below pages)
+	const MENU_ICON           = null;  // defaults to post icon
+	const META_CAP            = true;  // map_meta_cap
+	const ARCHIVE_PAGE        = true;
+	const EXPORTABLE          = true;
+	const TEXT_DOMAIN         = '';
+	const DEBUG               = false;
+	const DEBUG_PAGE          = 'debug';
+	const REST_BASE           = '';
+	const REST_CONTROLLER     = 'WP_REST_Posts_Controller';
 
 	public $labels;
 	public $capabilities;
@@ -101,17 +104,25 @@ class Custom_Post_Type_Controller {
 		$msg = static::SINGULAR_NAME . ' CPT: ';
 		try {
 			register_post_type( $this->lc_singular_name, $this->cpt_args );
-			if ( static::DEBUG && is_admin() ) {
-				$dump_msg = var_export( $this->cpt_args, true );
-				$am = new Admin_Message( $msg . $dump_msg );
-				$am->display_admin_normal_message();
-			} elseif ( static::DEBUG && is_page( 'debug' ) ) {
-				// http://dev.rdnap/debug/ example
-				var_dump( $this->cpt_args );
-			}
+			$this->debug_cpt();
 		} catch ( WP_Exception $e ) {
 			var_dump( $this->cpt_args );
 			return( true );
+		}
+	}
+
+	public function debug_cpt() {
+		// Displays the CPT setting in a standard WP admin message
+		if ( static::DEBUG && is_admin() ) {
+			$dump_msg = var_export( $this->cpt_args, true );
+			$am = new Admin_Message( $msg . $dump_msg );
+			$am->display_admin_normal_message();
+		} elseif ( static::DEBUG && is_page( static::DEBUG_PAGE ) ) {
+			/**
+			 * Displays the CPT setting in a WP page named debug (default)
+			 * @example http://MY-DOMAIN.com/debug/
+			 */
+			var_dump( $this->cpt_args );
 		}
 	}
 
@@ -125,11 +136,11 @@ class Custom_Post_Type_Controller {
 
 	public function set_labels() {
 		$this->labels = array(
-			'name' => _x( static::NAME, 'post type general name', static::TEXT_DOMAIN ),
-			'singular_name' => _x( static::SINGULAR_NAME, 'post type singular name', static::TEXT_DOMAIN ),
+			'name'               => _x( static::NAME, 'post type general name', static::TEXT_DOMAIN ),
+			'singular_name'      => _x( static::SINGULAR_NAME, 'post type singular name', static::TEXT_DOMAIN ),
 			'menu_name'          => _x( static::NAME, 'admin menu', static::TEXT_DOMAIN ),
 			'name_admin_bar'     => _x( static::SINGULAR_NAME, 'add new on admin bar', static::TEXT_DOMAIN ),
-			'add_new' => _x( 'Add New', $this->lc_singular_name, static::TEXT_DOMAIN ),
+			'add_new'            => _x( 'Add New', $this->lc_singular_name, static::TEXT_DOMAIN ),
 
 			'add_new_item'       => __( 'Add New ' . static::SINGULAR_NAME, static::TEXT_DOMAIN ),
 			'new_item'           => __( 'New ' . static::SINGULAR_NAME, static::TEXT_DOMAIN ),
@@ -161,21 +172,24 @@ class Custom_Post_Type_Controller {
 
 	public function set_cpt_args() {
 		$this->cpt_args = array(
-			'labels' => $this->labels,
-			'description' => __( static::DESCRIPTION, static::TEXT_DOMAIN ),
-			'public' => static::PUBLICLY_ACCESSIBLE,
-			'exclude_from_search' => static::EXCLUDE_SEARCH,
-			'publicly_queryable' => static::PUBLICLY_QUERYABLE,
-			'show_ui' => static::CUSTOM_ADMIN_UI,
-			'show_in_nav_menus' => static::ADD_TO_MENUS,
-			'show_in_menu' => static::ADD_TO_ADMIN_MENU,
-			'show_in_admin_bar' => static::ADD_TO_ADMIN_BAR,
-			'show_in_rest' => static::ADD_TO_REST_API,
-			'menu_position' => static::MENU_POSITION,
-			'menu_icon' => static::MENU_ICON,
-			'capability_type' => static::CAPABILITY_TYPE,
-			'map_meta_cap' => static::META_CAP,
-			'has_archive' => static::ARCHIVE_PAGE
+			'labels'                => $this->labels,
+			'description'           => __( static::DESCRIPTION, static::TEXT_DOMAIN ),
+			'public'                => static::PUBLICLY_ACCESSIBLE,
+			'exclude_from_search'   => static::EXCLUDE_SEARCH,
+			'publicly_queryable'    => static::PUBLICLY_QUERYABLE,
+			'show_ui'               => static::CUSTOM_ADMIN_UI,
+			'show_in_nav_menus'     => static::ADD_TO_MENUS,
+			'show_in_menu'          => static::ADD_TO_ADMIN_MENU,
+			'show_in_admin_bar'     => static::ADD_TO_ADMIN_BAR,
+			'show_in_rest'          => static::ADD_TO_REST_API,
+			'menu_position'         => static::MENU_POSITION,
+			'menu_icon'             => static::MENU_ICON,
+			'capability_type'       => static::CAPABILITY_TYPE,
+			'map_meta_cap'          => static::META_CAP,
+			'has_archive'           => static::ARCHIVE_PAGE,
+			'rest_base'             => static::REST_BASE,
+			'rest_controller_class' => static::REST_CONTROLLER,
+
 		);
 
 		if ( isset( $this->capabilities ) ) {
@@ -198,9 +212,9 @@ class Custom_Post_Type_Controller {
 
 	public function set_rewrite_args() {
 		$this->rewrite_args = array(
-			'slug' => $this->lc_name,
+			'slug'       => $this->lc_name,
 			'with_front' => false,
-			'feeds' => true
+			'feeds'      => true
 		);
 	}
 
@@ -208,7 +222,7 @@ class Custom_Post_Type_Controller {
 	 * Default taxonomy setting override in your child for alternatives
 	 */
 	public function set_taxonomies() {
-		$this->taxonomies = array( 'category', 'post_tag', 'brand' );
+		$this->taxonomies = array( 'category', 'post_tag' );
 	}
 
 	/**
