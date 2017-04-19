804dccce323edeb77f17145711597c177b5702b9
Mikel King <mikel.king@olivent.com>
2017-04-19 09:22:15 -0400

    Refactored the cpt controller yet again.

--- 020-cpt-controller.php
+++ 020-cpt-controller.php
@@ -111,10 +111,21 @@ class Custom_Post_Type_Controller {
 		}
 	}
 
-	public function debug_cpt( Custom_Post_Type_Controller $cpt = null, $msg = null ) {
+	/**
+	 * A basic debugging method
+	 * @param null $cpt
+	 * @param null $msg
+	 */
+	public function debug_cpt( $cpt = null, $msg = null ) {
+		if ( $cpt ) {
+			$object = $cpt;
+		} else {
+			$object = $this;
+		}
+
 		// Displays the CPT setting in a standard WP admin message
 		if ( static::DEBUG && is_admin() ) {
-			$dump_msg = var_export( $cpt->cpt_args, true );
+			$dump_msg = var_export( $object->cpt_args, true );
 			$am = new Admin_Message( $msg . $dump_msg );
 			$am->display_admin_normal_message();
 		} elseif ( static::DEBUG && is_page( static::DEBUG_PAGE ) ) {
@@ -122,7 +133,7 @@ class Custom_Post_Type_Controller {
 			 * Displays the CPT setting in a WP page named debug (default)
 			 * @example http://MY-DOMAIN.com/debug/
 			 */
-			var_dump( $cpt->cpt_args );
+			var_dump( $object->cpt_args );
 		}
 	}
 
