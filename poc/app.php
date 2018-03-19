7ad5431756a51d9a38d2b10b9dcda5742e8f1c42
Mikel King <mikel.king@olivent.com>
2018-03-18 20:07:59 -0400

    Implemented an asset handling trait
    Refactoring to increase the utiliaztion of traits and
    interfaces to streamline bacon as part of the 2.0 reoadmap.

--- poc/app.php
+++ poc/app.php
@@ -1,11 +1,12 @@
 <?php
 
+require( '../000-asset-handler-trait.php' );
 require( '../010-asset-base.php' );
 require( 'ad-asset.php' );
 require( 'test-asset.php' );
 
-$aa = new AdAsset;
+$aa = new Ad_Asset;
 $aa->render_localized_data();
 
-$ta = new TestAsset;
+$ta = new Test_Asset;
 $ta->render_localized_data();
